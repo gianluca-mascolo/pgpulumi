cc307aa653f64a420a9f660b9ef949beef46a7c3
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-19 19:37:25 +0100

    create db users

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -3,5 +3,24 @@
 import pulumi
 
 import pulumi_postgresql as postgresql
+import pulumi_random as random
 
-my_db = postgresql.Database(resource_name="my-database",name="my-database")
+conf = pulumi.Config("pgpulumi")
+dblist = conf.require_object("databases")
+dbgroups = conf.require_object("groups")
+
+for db in dblist:
+    postgresql.Database(resource_name=db,name=db)
+
+allusers=[]
+for g in dbgroups:
+    allusers.extend(g["users"])
+allusers=set(allusers)
+
+for username in allusers:
+    postgresql.Role(resource_name=f"{username}-role",name=username,login=True,password=random.RandomPassword(f"{username}-password",length=20))
+# for g in dbgroups:
+#     print(g["name"])
+#     print(g["users"][0])
+#     print(g["database"][0]["name"])
+
