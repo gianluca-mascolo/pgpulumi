5210fa64c92a0d68c6d3d83f422c4cdab66133e6
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-19 22:04:09 +0100

    commit

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -4,7 +4,7 @@ import pulumi
 
 import pulumi_postgresql as postgresql
 import pulumi_random as random
-
+from pulumi import Output
 conf = pulumi.Config("pgpulumi")
 dblist = conf.require_object("databases")
 dbgroups = conf.require_object("groups")
@@ -18,7 +18,15 @@ for g in dbgroups:
 allusers=set(allusers)
 
 for username in allusers:
-    postgresql.Role(resource_name=f"{username}-role",name=username,login=True,password=random.RandomPassword(f"{username}-password",length=20))
+    userpassword=random.RandomPassword(f"{username}-password",length=20)
+    postgresql.Role(resource_name=f"{username}-role",name=username,login=True,password=userpassword.result)
+    #userpasswordoutput=userpassword.get(resource_name=f"{username}-password-output",id=f"{username}-password")
+    #random.RandomPassword.get(resource_name=f"{username}-password-output",id=f"{username}-password")
+    # userpasswordouput=str(Output.all(userpassword).apply(lambda result: f"{result}"))
+    # pulumi.export(
+    #     f"{username}-credentials",
+    #     f"{username}:{userpasswordouput}"
+    # )
 # for g in dbgroups:
 #     print(g["name"])
 #     print(g["users"][0])
