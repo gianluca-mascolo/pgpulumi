a3bdd207d312d78ac54f4c395380ebadc87d1f3d
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-20 10:32:55 +0100

    init  database in docker

---
+++ initdb.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wonderfuldb;
+--CREATE TABLE wonderfuldb.public.test_table_1 (varie VARCHAR (10));
+CREATE DATABASE sarchiapone;
+--CREATE TABLE sarchiapone.public.tabellina (varie VARCHAR (10));
