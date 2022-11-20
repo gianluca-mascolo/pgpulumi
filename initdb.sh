f66b180e67cb7cf5ab48597977e8a4a4d407da0f
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-20 19:20:30 +0100

    init db via docker with tables

---
+++ initdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE wonderfuldb;
+	CREATE DATABASE sarchiapone;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "wonderfuldb" <<-EOSQL
+	CREATE TABLE test_table_1 (varie VARCHAR (10));
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sarchiapone" <<-EOSQL
+	CREATE TABLE tabellina (varie VARCHAR (10));
+EOSQL
--- initdb.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wonderfuldb;
---CREATE TABLE wonderfuldb.public.test_table_1 (varie VARCHAR (10));
-CREATE DATABASE sarchiapone;
---CREATE TABLE sarchiapone.public.tabellina (varie VARCHAR (10));
