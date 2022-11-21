a6745f8ea70a675a69ed54e07c6a1d1723d7cf32
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-21 21:52:32 +0100

    add some sample values to sample tables

--- initdb.sh
+++ initdb.sh
@@ -8,8 +8,10 @@ EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "wonderfuldb" <<-EOSQL
 	CREATE TABLE test_table_1 (varie VARCHAR (10));
+	INSERT INTO test_table_1 VALUES ( 'aaaa' );
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sarchiapone" <<-EOSQL
 	CREATE TABLE tabellina (varie VARCHAR (10));
+	INSERT INTO tabellina VALUES ( 'bbbb' );
 EOSQL
