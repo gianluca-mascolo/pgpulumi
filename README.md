b694bbf38b19010e556f773a5aa2778e8ef25903
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-11-23 15:00:54 +0100

    implicit dependencies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 pulumi import postgresql:index/role:Role pluto-role pluto
+
+pulumi stack output --show-secrets pwexport-ciccio
--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -8,35 +8,32 @@ from pulumi import Output
 conf = pulumi.Config("pgpulumi")
 dbgroups = conf.require_object("groups")
 permissionsets = conf.require_object("permissionsets")
-debug = conf.get_bool("debug") or False
-
-def write_to_file(u,p):
-    f = open("userlist.txt", "a")
-    f.write(f"{u}:{p}\n")
-    f.close()
 
 allusers=[]
+group_resource={}
 for group in dbgroups:
     allusers.extend(group["users"])
-    postgresql.Role(resource_name=f"{group['name']}-group-role",name=group["name"],login=False)
+    group_name=group['name']
+    group_resource[group_name]=postgresql.Role(resource_name=f"{group_name}-group-role",name=group_name,login=False)
     for db in group["databases"]:
         privileges=permissionsets[db["privileges"]]
         postgresql.Grant(
-            resource_name=f'{group["name"]}-{db["name"]}-{db["privileges"]}-privileges',database=db["name"],
+            resource_name=f'{group_name}-{db["name"]}-{db["privileges"]}-privileges',database=db["name"],
             object_type="table",
             privileges=privileges,
-            role=group["name"],
+            role=group_resource[group_name].name,
             schema="public")
 
 allusers=set(allusers)
 
-if debug:
-    f = open("userlist.txt", "w")
-    f.close()
-
 for username in allusers:
     userpassword=random.RandomPassword(f"{username}-password",length=8,special=False)
-    userroles=[group["name"] for group in dbgroups if username in group["users"] ]
-    postgresql.Role(resource_name=f"{username}-role",name=username,login=True,password=userpassword.result,roles=userroles)
-    if debug:
-        Output.all(username,userpassword.result).apply(lambda args: write_to_file(u=args[0],p=args[1]))
+    usergroups=[group["name"] for group in dbgroups if username in group["users"] ]
+    postgresql.Role(
+        resource_name=f"{username}-role",
+        name=username,
+        login=True,
+        password=userpassword.result,
+        roles=[group_resource[group_name].name for group_name in usergroups ]
+        )
+    pulumi.export(f"pwexport-{username}", userpassword.result)
